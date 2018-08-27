f2ee3b4d353206a478c6c39d7831a261c2430171
qinqiang1990 <1247686880@qq.com>
2018-08-27 17:41:49 +0800

    m

--- ctc/ocr_predict.py
+++ ctc/ocr_predict.py
@@ -48,10 +48,7 @@ if __name__ == '__main__':
         basemodel = Model(inputs=model.get_layer('the_input').output, outputs=model.get_layer('dense_1').output)
         data_, label_ = get_data(image_height=img_height, image_width=img_width)
         pred_ = predict_model(basemodel, data_)
-        res = label_-pred_
-        min_res = res[:,3:7]
+        res = label_ - pred_
+        min_res = res[:, 3:7]
         print(res)
         print(min_res)
-        
-
-        
