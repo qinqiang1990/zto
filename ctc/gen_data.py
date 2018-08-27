c9ae327e3b33158cfa176098cbe68269e4031141
qinqiang1990 <1247686880@qq.com>
2018-08-27 16:31:13 +0800

    m

--- ctc/gen_data.py
+++ ctc/gen_data.py
@@ -40,18 +40,18 @@ def gen_hand_write(batch_size=32, n_len=11):
     y = np.zeros((batch_size, n_len), dtype=np.uint8)
     for i in range(batch_size):
         if i == 0:
-            random_text = "18852890100"
+            number = [1, 8, 8, 5, 2, 8, 9, 0, 1, 0, 0]
         elif i == 1:
-            random_text = "17224537850"
+            number = [1, 7, 2, 2, 4, 5, 3, 7, 8, 5, 0]
         elif i == 2:
-            random_text = "12643790432"
+            number = [1, 2, 6, 4, 3, 7, 9, 0, 4, 3, 2]
         elif i == 3:
-            random_text = "53791628492"
+            number = [5, 3, 7, 9, 1, 6, 2, 8, 4, 9, 2]
         elif i == 4:
-            random_text = "86412583568"
+            number = [8, 6, 4, 1, 2, 5, 8, 3, 5, 6, 8]
         else:
             number = np.random.choice(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], n_len)
-            random_text = "".join(number.astype(np.unicode))
+        random_text = "".join(number.astype(np.unicode))
         image_data = hand_write.get_img(str=random_text, run=True, font_path=None)
         image_data = cv2.adaptiveThreshold(image_data, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, -10)
         image_data = cv2.resize(image_data, (width, height), interpolation=cv2.INTER_AREA)
