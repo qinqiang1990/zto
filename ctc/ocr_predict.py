6b7be2edf5ebc98e46be27075d4f9ce9fb5e4926
qinqiang1990 <1247686880@qq.com>
2018-09-14 16:30:16 +0800

    m

--- ctc/ocr_predict.py
+++ ctc/ocr_predict.py
@@ -20,16 +20,18 @@ def predict_model(model, input_):
     return output_
 
 
-def get_data(path="data/cut/", image_height=20, image_width=140):
+def get_data(path="data/cut/", image_height=32):
     data = []
     label = []
     files = os.listdir(path)
     for file in files:
         file_path = os.path.join(path, file)
         img = cv2.imread(file_path)
-        img = common.resize_(img, width=image_width, height=image_height)
         img = common.bgr2gray_(img)
-        img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, -10)
+        h, w = img.shape[:2]
+        img = cv2.resize(img, (int(w / h * image_height), image_height), interpolation=cv2.INTER_AREA)
+        img = cv2.equalizeHist(img)
+        # img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, -10)
         data.append(img[:, :, np.newaxis])
         label.append(list(map(int, file.split('.')[0])))
     return np.array(data), np.array(label)
@@ -38,7 +40,6 @@ def get_data(path="data/cut/", image_height=20, image_width=140):
 if __name__ == '__main__':
 
     img_height = int(mod_config.getConfig("train", "img_height"))
-    img_width = int(mod_config.getConfig("train", "img_width"))
 
     model = ocr.build_network(image_height=img_height, image_width=None)
 
@@ -47,7 +48,7 @@ if __name__ == '__main__':
     if os.path.exists(weight_file):
         model.load_weights(weight_file)
         basemodel = Model(inputs=model.get_layer('the_input').output, outputs=model.get_layer('softmax').output)
-        data_, label_ = get_data(image_height=img_height, image_width=img_width)
+        data_, label_ = get_data(image_height=img_height, image_width=None)
         pred_ = predict_model(basemodel, data_)
         print("origin:")
         print(label_)
